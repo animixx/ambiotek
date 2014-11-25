efd1dd20c6624afb00380e7faeccdb915e7b4d69
animix <animix@vtr.net>
2014-11-24 23:52:07 -0700

    mapa satelital, falta ubicar el punto

--- src/Eye3/CaminosBundle/Controller/MapaController.php
+++ src/Eye3/CaminosBundle/Controller/MapaController.php
@@ -5,7 +5,10 @@ namespace Eye3\CaminosBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Ivory\GoogleMap\Map,
+	Ivory\GoogleMap\MapTypeId,
+    Ivory\GoogleMap\Overlays\Marker;
+	
 class MapaController extends Controller
 {
     /**
@@ -14,8 +17,36 @@ class MapaController extends Controller
      */
     public function indexAction()
     {
+	
+		$map = new Map();
+
+		// Enable the auto zoom flag
+		$map->setAutoZoom(false);
+		
+		$map->setCenter(0, 0, true);
+		$map->setMapOption('zoom', 5);
+		
+		$map->setBound(-21, -62, -22, -63, true, true);
+		
+		$map->setMapOption('mapTypeId', MapTypeId::SATELLITE);
+		$map->setMapOption('mapTypeId', 'satellite');
+
+		$map->setMapOption('mapTypeId', MapTypeId::TERRAIN);
+		$map->setMapOption('mapTypeId', 'terrain');
+
+		// Add marker overlay to your map
+		$map->addMarker(new Marker());
+		
+		
+		$map->setStylesheetOptions(array(
+			'width'  => '800px',
+			'height' => '500px'
+		));
+
+		$map->setLanguage('es');
+
         return array(
-                // ...
+               'map' => $map,
             );    }
 
 }
